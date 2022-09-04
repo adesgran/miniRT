d19a1bbb41b82521c0b01955ee1763c371c6b4cc
Antoine DESGRANGES <adesgran@student.42.fr>
2022-09-04 15:43:23 +0200

    Fix make

--- utils/geometry_utils.c
+++ utils/geometry_utils.c
@@ -6,7 +6,7 @@
 /*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 13:30:35 by adesgran          #+#    #+#             */
-/*   Updated: 2022/09/04 15:40:12 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/09/04 15:42:38 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,28 +46,14 @@ double	max(double a, double b)
 void	vector_product(t_coord *a, t_coord *b, t_coord *res)
 {
 	double	coeff;
-	t_line	la;
-	t_line	lb;
 
-	//printf("A\tx=%f y=%f z=%f\n", a->x, a->y, a->z);
-	//printf("B\tx=%f y=%f z=%f\n", b->x, b->y, b->z);
-	la.dir.x = a->x;
-	la.dir.y = a->y;
-	la.dir.z = a->z;
-	lb.dir.x = b->x;
-	lb.dir.y = b->y;
-	lb.dir.z = b->z;
 	coeff = sqrt(pow(a->x, 2) + pow(a->y, 2) + pow(a->z, 2));
 	coeff *= sqrt(pow(b->x, 2) + pow(b->y, 2) + pow(b->z, 2));
-	coeff *= sin(get_angle(&la, &lb));
+	coeff *= sin(get_angle(a, b));
 	res->x = a->y * b->z - a->z * b->y;
 	res->y = a->z * b->x - a->x * b->z;
 	res->z = a->x * b->y - a->y * b->x;
-	//printf("Res\tx=%f y=%f z=%f\n", res->x, res->y, res->z);
-	//printf("Coeff = %f", coeff);
-	//printf("   x = %f", res->x);
 	norm_vector(res);
-	//printf("   x = %f\n", res->x);
 	res->x = res->x * coeff;
 	res->y *= coeff;
 	res->z *= coeff;
