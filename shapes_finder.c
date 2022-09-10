6c58355179a29e8f2ed02a7a1e41709b0a63d93f
Mathilde CHASSIGNET <mchassig@e1r2p17.clusters.42paris.fr>
2022-09-10 18:04:44 +0200

    test vecteur plan

--- shapes_finder.c
+++ shapes_finder.c
@@ -6,7 +6,7 @@
 /*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 13:48:57 by mchassig          #+#    #+#             */
-/*   Updated: 2022/09/07 13:32:26 by mchassig         ###   ########.fr       */
+/*   Updated: 2022/09/10 18:01:53 by mchassig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@ double	plan_finder(t_shapes *shape, t_line *line)
 	t_plan	*plan;
 	double	u;
 	double	d;
+	double	a;
+	double	coef;
 	
 	norm_vector(&line->dir);
 	plan = (t_plan *)shape->content;
-	if ((get_angle(&line->dir, &plan->dir) - M_PI / 2) < 0.001)
+	a = get_angle(&line->dir, &plan->dir);
+	if (fabs(a - M_PI / 2) < 0.001)
 		return (-1);
+	if (fabs(a) < 3 * M_PI / 2)
+		coef = -1;
+	else
+		coef = 1;
 	d = - (plan->dir.x * plan->pos.x + plan->dir.y * plan->pos.y + plan->dir.z * plan->pos.z);
 	u = - (line->pos.x * plan->dir.x + line->pos.y * plan->dir.y + line->pos.z * plan->dir.z + d);
 	u /= plan->dir.x * line->dir.x + plan->dir.y * line->dir.y + plan->dir.z * line->dir.z;
@@ -30,9 +37,9 @@ double	plan_finder(t_shapes *shape, t_line *line)
 	shape->norm.pos.x = line->pos.x + u * line->dir.x;
 	shape->norm.pos.y = line->pos.y + u * line->dir.y;
 	shape->norm.pos.z = line->pos.z + u * line->dir.z;
-	shape->norm.dir.x = plan->dir.x;
-	shape->norm.dir.y = plan->dir.y;
-	shape->norm.dir.z = plan->dir.z;
+	shape->norm.dir.x = plan->dir.x * coef;
+	shape->norm.dir.y = plan->dir.y * coef;
+	shape->norm.dir.z = plan->dir.z * coef;
 	return (u);
 }
 
