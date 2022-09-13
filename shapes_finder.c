4c8fba7ff9c24e1256d722d0ab1610f1501a6bd0
Mathilde CHASSIGNET <mchassig@e1r5p14.42.fr>
2022-09-13 11:33:37 +0200

    fix merge conflicts (for real this time)

--- shapes_finder.c
+++ shapes_finder.c
@@ -6,50 +6,12 @@
 /*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 13:48:57 by mchassig          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/09/10 18:01:53 by mchassig         ###   ########.fr       */
-=======
-/*   Updated: 2022/09/11 17:05:29 by adesgran         ###   ########.fr       */
->>>>>>> ecab62086d01e202ed42da3fec50321aee04fa96
+/*   Updated: 2022/09/13 11:32:32 by mchassig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
 
-<<<<<<< HEAD
-double	plan_finder(t_shapes *shape, t_line *line)
-{
-	t_plan	*plan;
-	double	u;
-	double	d;
-	double	a;
-	double	coef;
-	
-	norm_vector(&line->dir);
-	plan = (t_plan *)shape->content;
-	a = get_angle(&line->dir, &plan->dir);
-	if (fabs(a - M_PI / 2) < 0.001)
-		return (-1);
-	if (fabs(a) < 3 * M_PI / 2)
-		coef = -1;
-	else
-		coef = 1;
-	d = - (plan->dir.x * plan->pos.x + plan->dir.y * plan->pos.y + plan->dir.z * plan->pos.z);
-	u = - (line->pos.x * plan->dir.x + line->pos.y * plan->dir.y + line->pos.z * plan->dir.z + d);
-	u /= plan->dir.x * line->dir.x + plan->dir.y * line->dir.y + plan->dir.z * line->dir.z;
-	if (u < 0)
-		return (-1);
-	shape->norm.pos.x = line->pos.x + u * line->dir.x;
-	shape->norm.pos.y = line->pos.y + u * line->dir.y;
-	shape->norm.pos.z = line->pos.z + u * line->dir.z;
-	shape->norm.dir.x = plan->dir.x * coef;
-	shape->norm.dir.y = plan->dir.y * coef;
-	shape->norm.dir.z = plan->dir.z * coef;
-	return (u);
-}
-
-=======
->>>>>>> ecab62086d01e202ed42da3fec50321aee04fa96
 unsigned int	shapes_finder(t_env *env, t_shapes *shapes, t_line *line)
 {
 	double			u;
